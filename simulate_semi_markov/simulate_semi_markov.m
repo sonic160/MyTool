505ed023186c77decbd507659ce27c1300e7b3bb
sonic160 <zhiguo.zeng@centralesupelec.fr>
2019-12-05 13:41:08 +0100

    Creation
    - Code for simulating semi-Markov  process.
    - Test it with a reference case study.

---
+++ simulate_semi_markov/simulate_semi_markov.m
@@ -0,0 +1,48 @@
+% This is to simulate a trajectory of a homogeneous semi-Markov process.
+% Input: Q - A handle to a function that generate values of Q_{i,j}(t).
+%        P - Transition probability matrix of the embedded Markov chain.
+%        pi - Initial distribution.
+%        T_max - Evaluation horizon.
+% Output: t - times of the jumps.
+%         y - the state after the jumps.
+% History:  20191205 - Created by ZZ
+
+function [t,y] = simulate_semi_markov(Q_t,P,pi,T_max)
+t(1) = 0; % start times at 0
+y(1) = rando(pi); % generate the initial y
+
+i = 1;
+while 1
+    % Generate next state
+    y_next = rando(P(y(i),:)); % Simulate next state using the transitin matrix of the embedded chain
+    % Generate holding time
+    r = rand; % A random number from uniform dist
+    eq = @(t) cal_holding_time_cdf(t,Q_t,P,y(i),y_next) - r; % Equation for inverse function of holding time cdf
+    t_next = fzero(eq,0); % Inverse function method to generate random number
+    t_cur = t(i) + t_next; % Update time
+    if t_cur < T_max % If this transition happens in the evaluation horizon
+        % Update the parameters
+        t(i+1) = t_cur;
+        y(i+1) = y_next;
+        i = i+1;
+    else
+        break;
+    end
+end
+
+%  rando.m generates a random variable in 1, 2, ..., n given a distribution 
+%  vector. 
+function index = rando(p)
+u = rand;
+i = 1;
+s = p(1);
+while ((u > s) && (i < length(p)))
+    i=i+1;
+    s=s+p(i);
+end
+index=i;
+
+% This is to calculate the holding time distribution
+function cdf_ht_t = cal_holding_time_cdf(t,Q_t,P,y_prev,y_next)
+F = Q_t(t)./P;
+cdf_ht_t = F(y_prev,y_next);
---
+++ simulate_semi_markov/test_simulation.m
@@ -0,0 +1,69 @@
+% This is a test of the simulation algorithm.
+% Benchmark: A two state system.
+% Time to failure: Weibull, eta = 10, beta = 1.1
+% Time to repair: Lognormal, mu = 1, sigma = 0.1
+% Last edit: 20191204 - Created by ZZ
+clear; clc;
+%% Create benchmark
+a = 10; b = 1.1;
+pd_t_f = makedist('weibull','a',a,'b',b); % Time to failure distribution
+mu = 1; sigma = .1;
+pd_t_r = makedist('lognormal','mu',mu,'sigma',sigma); % Time to repair distribution
+T_max = 20;
+t = linspace(.1,30,T_max); % Evaluate time
+ns = 1e3; % Sample size
+A_t_ref = zeros(1,length(t)); % Availability
+for i = 1:length(t)
+    fprintf('%d/%d\n',i,length(t));
+    count = 0; % Counter
+    for j = 1:ns
+        t_cur = 0; % Initial value of time
+        state = 0; % Start from working
+        while 1
+            if state == 0 % if current state is working
+                t_next = pd_t_f.random(); % Generate time to failure
+                t_cur = t_cur + t_next; % Update current time
+                if t_cur > t(i) % If beyond simulation horizon
+                    count = count + 1;
+                    break;
+                else
+                    state = 1; % System fails
+                end
+            else
+                t_next = pd_t_r.random(); % Generate time to failure
+                t_cur = t_cur + t_next; % Update current time
+                if t_cur > t(i) % If beyond simulation horizon
+                    break;
+                else
+                    state = 0; % System fails
+                end
+            end
+        end       
+    end
+    A_t_ref(i) = count/ns;
+end
+figure
+plot(t,A_t_ref,'-o')
+hold on
+%% Run the simulation using the algorithm being tested
+pi = [1,0];
+Q_01 = @(t) wblcdf(t,a,b);
+Q_10 = @(t) logncdf(t,mu,sigma);
+Q_t = @(t) [0, Q_01(t);...
+    Q_10(t),0];
+P = [0, 1;...
+    1, 0];
+% Calculate the same availability
+A_t_sim = zeros(1,length(t)); % Availability
+for i = 1:length(t)
+    fprintf('%d/%d\n',i,length(t));
+    count = 0; % Counter
+    for j = 1:ns
+        [temp_t,y] = simulate_semi_markov(Q_t,P,pi,t(i));
+        if y(end) == 1
+            count = count + 1;
+        end
+    end
+    A_t_sim(i) = count/ns;
+end
+plot(t,A_t_sim,'-dr')
