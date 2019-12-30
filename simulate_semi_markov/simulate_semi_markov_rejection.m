2006364ece72896cd3f9e4555a69ca4bd22c27ad
sonic160 <zhiguo.zeng@centralesupelec.fr>
2019-12-30 14:58:07 +0100

    Extend the rejection method to nonhomogeneous SMP
    - Change simulate_semi_markov_rejection to cope with nonhomogeneous SMP.
    - Change test_semi_Markov_2 to add a comparison to the rejection-based sampling.

--- simulate_semi_markov/simulate_semi_markov_rejection.m
+++ simulate_semi_markov/simulate_semi_markov_rejection.m
@@ -1,43 +1,101 @@
 % This is to simulate a trajectory of a homogeneous semi-Markov process.
 % Rejection method is used for generating random samples
-% Input: f_t - A handle to a function that generate values of f_{i,j}(t).
-%        f_{i,j}(t) is the pdf of the corresponding holding time
-%        distributions
-%        max_value - Maximum of the pdf in its support. A n*n matrix.
-%        support - A vector [lower,upper] that defines the support of the
-%        pdf. A n*n cell matrix.
+% Input: f_t - A matrix that contains handles to a function that generate values of f_{i,j}(t,tau).
+%        f_{i,j}(t,tau) is the pdf of the corresponding holding time
+%        distributions, where t is the holding time, tau is the age.
+%        For homogeneous semi-Markov chain, tau = 0.
+%        g_t - A matrix that contains handles to the proposal densities.
+%        g_t{i,j} gives a handle, whose parameters are t and tau.
+%        g_rng_t - A matrix that contains handles to the random number
+%        generator.
+%        c_t - The upper bound constant.
 %        P - Transition probability matrix of the embedded Markov chain.
 %        pi - Initial distribution.
 %        T_max - Evaluation horizon.
+%        IS_NHSMP - 1, indicates NHSMP
+%                 - 0, homogeneous SMP. Note: when IS_NHSMP is 0, the
+%                 hanldes in f_t, g_t, g_rng_t, c_t only have one parameter
+%                 t.
 % Output: t - times of the jumps.
 %         y - the state after the jumps.
-% History: 20191223 - Update the rejection sampling subfunction.
+% History: 20191223b - Add a branch for nonhomogeneous semi-Markov process.
+%          20191223 - Update the rejection sampling subfunction.
 %                     Extend to be applicable using proposal density.
 %          20191221 - Created by ZZ
 
-function [t,y] = simulate_semi_markov_rejection(f_t,g_t,g_rng_t,c_t,P,pi,T_max)
-    t(1) = 0; % start times at 0
-    y(1) = rando(pi); % generate the initial y
+function [t,y] = simulate_semi_markov_rejection(f_t,g_t,g_rng_t,c_t,P,pi,T_max,IS_NHSMP)
+    % Check it is a homogeneous or nonhomogeneous semi-Markov model
+    if nargin == 8 && IS_NHSMP == 1 % Nonhomogeneous
+        t(1) = 0; % start times at 0
+        y(1) = rando(pi); % generate the initial y
 
-    i = 1; % Index of the current state
-    while 1
-        % Generate next state
-        y_next = rando(P(y(i),:)); % Simulate next state using the transitin matrix of the embedded chain
-        % Generate holding time
-        pdf_holding_time = f_t{y(i),y_next}; % handle to the holding time distribution pdf
-        prop_den = g_t{y(i),y_next}; % Handle to the proposal density.
-        prop_rng = g_rng_t{y(i),y_next}; % Handle for generating samples from the proposal density.
-        c = c_t(y(i),y_next);
-        t_next = accrejrnd(pdf_holding_time,prop_den,prop_rng,c,1,1); % Generate random number from pdf.
-        t_cur = t(i) + t_next; % Update time
-        if t_cur < T_max % If this transition happens in the evaluation horizon
-            % Update the parameters
-            t(i+1) = t_cur;
-            y(i+1) = y_next;
-            i = i+1;
-        else
-            break;
+        i = 1; % Index of the current state
+        while 1
+            tau = t(i);
+            % Generate next state
+            n_state = length(P);
+            p_next_state = zeros(1,n_state);
+            for j = 1:n_state
+                handle_p = P{y(i),j};
+                p_next_state(j) = handle_p(tau);
+            end
+            y_next = rando(p_next_state); % Simulate next state using the transitin matrix of the embedded chain
+                        
+            % handle to the holding time distribution pdf
+            temp_f_t = f_t{y(i),y_next}; 
+            pdf_holding_time = @(x) temp_f_t(x,tau);
+            
+            % Handle to the proposal density.
+            temp_g_t = g_t{y(i),y_next}; 
+            prop_den = @(x) temp_g_t(x,tau);
+            
+            % Handle for generating samples from the proposal density.
+            temp_rng = g_rng_t{y(i),y_next}; 
+            prop_rng = @() temp_rng(tau); 
+            
+            % Calculate c
+            handle_c = c_t{y(i),y_next};
+            c = handle_c(tau);
+            
+            t_next = accrejrnd(pdf_holding_time,prop_den,prop_rng,c,1,1); % Generate random number from pdf.
+            t_cur = t(i) + t_next; % Update time
+            if t_cur < T_max % If this transition happens in the evaluation horizon
+                % Update the parameters
+                t(i+1) = t_cur;
+                y(i+1) = y_next;
+                i = i+1;
+            else
+                t(i+1) = T_max;
+                y(i+1) = y(i);
+                break;
+            end
         end
+     else % Homogeneous   
+        t(1) = 0; % start times at 0
+        y(1) = rando(pi); % generate the initial y
+
+        i = 1; % Index of the current state
+        while 1
+            % Generate next state
+            y_next = rando(P(y(i),:)); % Simulate next state using the transitin matrix of the embedded chain
+            % Generate holding time
+            pdf_holding_time = f_t{y(i),y_next}; % handle to the holding time distribution pdf
+            prop_den = g_t{y(i),y_next}; % Handle to the proposal density.
+            prop_rng = g_rng_t{y(i),y_next}; % Handle for generating samples from the proposal density.
+            c = c_t(y(i),y_next);
+            t_next = accrejrnd(pdf_holding_time,prop_den,prop_rng,c,1,1); % Generate random number from pdf.
+            t_cur = t(i) + t_next; % Update time
+            if t_cur < T_max % If this transition happens in the evaluation horizon
+                % Update the parameters
+                t(i+1) = t_cur;
+                y(i+1) = y_next;
+                i = i+1;
+            else
+                t(i+1) = T_max;
+                y(i+1) = y(i);
+                break;
+            end
+        end               
     end
 end
 
--- simulate_semi_markov/test_semi_Markov_2.m
+++ simulate_semi_markov/test_semi_Markov_2.m
@@ -6,7 +6,7 @@
 %            State 1: Dummy perfect state.
 %            State 2: Performance degradation.
 %            State 3: Total failure (absorption state).
-% Last edit: 20191221 - Created by ZZ
+% Last edit: 20191223 - Created by ZZ
 clear; clc;
 %% Create benchmark
 % Parameter definition
@@ -21,11 +21,14 @@ pd_t_f_s = makedist('weibull','a',eta_s,'b',beta_s); % Time to failure distribut
 eta_r = 1; beta_r = 2;
 pd_t_r = makedist('weibull','a',eta_r,'b',beta_r); 
 
-% Simulation to calculate p(t) for each state, for 0<t<100
-T_max = 10000;
+% Simulation to calculate p(t) for each state, for 0<t<T_max
+T_max = 5e3;
 t = linspace(.1,T_max,10); % Evaluate time
 ns = 1e5; % Sample size
 p_t_ref = zeros(4,length(t)); % p(t): 4*1, each column corresponds to one state
+
+tic;
+
 for i = 1:length(t)
     fprintf('%d/%d\n',i,length(t));
     count_0 = 0; count_1 = 0; count_2 = 0; count_3 = 0; % Counter for ending at each state
@@ -118,6 +121,9 @@ for i = 1:length(t)
     p_t_ref(3,i) = count_2/ns;
     p_t_ref(4,i) = count_3/ns;
 end
+
+elapsed_time_1 = toc;
+
 % Display results
 h_p_1 = figure();
 plot(t,p_t_ref(1,:),'-o')
@@ -132,3 +138,133 @@ h_p_4 = figure();
 plot(t,p_t_ref(4,:),'-o')
 hold on
 
+%% Simulate using holding time distribution and embedded chain. Sampling with rejection method.
+% Define the embedded chain and the holding time dist.
+pi = [1,0,0,0]; % Initial distribution
+p_01 = @(tau) (-0.887024E-1).*exp(1).^((1/1000).*tau).*...
+    ((-0.1E1)+erf((1/100).*(5+tau)));
+p_02 = @(tau) 0.9984E0+0.887024E-1.*exp(1).^((1/1000).*tau).*((-0.1E1)+...
+    erf(0.5E-1+ 0.1E-1.*tau));
+p_03 = @(tau) .16e-2;
+p_10 = p_01;
+p_12 = p_02;
+p_13 = p_03;
+p_20 = @(tau) .999114;
+p_23 = @(tau) .000885727;
+handle_zero = @(tau) 0;
+handle_one = @(tau) 1;
+P = {handle_zero, p_01, p_02, p_03;...
+    p_10, handle_zero, p_12, p_13;...
+    p_20, handle_zero, handle_zero, p_23;...
+    handle_zero, handle_zero, handle_zero, handle_one}; % Transition probabability matrix
+
+% Holding time distributions
+f_01 = @(x,tau) (-0.112556E-1).*exp(1).^((-1/1000).*tau+...
+    (1/10000).*((-10).*x+(-1).*(x+tau).^2)).*((-0.1E1)+erf((1/100).*(5+tau))).^(-1);
+f_02 = @(x,tau) 0.9984E-3.*exp(1).^((-1/1000).*x).*(1+(-1).*exp(1).^((-1/10000).*...
+    (x+tau).^2)).*(0.9984E0+0.887024E-1.*exp(1).^((1/1000).*tau).*...
+    ((-0.1E1)+erf(0.5E-1+0.1E-1.*tau))).^(-1);
+f_03 = @(x,tau) 0.1E-2.*exp(1).^((-1/1000).*x);
+f_10 = f_01;
+f_12 = f_02;
+f_13 = f_03;
+f_20 = @(x,tau) 0.200177E1.*exp(1).^((-1/1000).*x+(-1).*x.^2).*x;
+f_23 = @(x,tau) 0.112902E1.*exp(1).^((-1/1000).*x+(-1).*x.^2);
+handle_zero_2_par = @(t,tau) 0;
+handle_one_2_par = @(t,tau) 1;
+f_t = {handle_zero_2_par, f_01, f_02, f_03;...
+    f_10, handle_zero_2_par, f_12, f_13;...
+    f_20, handle_zero_2_par, handle_zero_2_par, f_23;...
+    handle_one_2_par, handle_one_2_par, handle_one_2_par, handle_one_2_par};
+
+% Proposal density
+g_01 = f_01; % Use inverse function method for this element.
+g_03 = f_03; % Use inverse function method for this element.
+g_23 = f_23; % Use inverse function method for this element.
+
+g_02 = @(t,tau) pd_disp.pdf(t);
+g_20 = @(t,tau) pd_t_r.pdf(t);
+
+g_t = {handle_zero_2_par, g_01, g_02, g_03;...
+    g_01, handle_zero_2_par, g_02, g_03;...
+    g_20, handle_zero_2_par, handle_zero_2_par, g_23;...
+    handle_one_2_par, handle_one_2_par, handle_one_2_par, handle_one_2_par};
+
+% Random number generator for the proposal density
+temp_g_rng_01 = @(p,tau) -5 - tau + 100*erfinv(p + (1-p).*erf(.05+.01*tau));
+g_rng_01 = @(tau) temp_g_rng_01(rand(),tau);
+
+temp_g_rng_23 = @(p,tau) (-1 + 2000*erfinv(p + erf(1/2000) - p.*erf(1/2000)))/2000;
+g_rng_23 = @(tau) temp_g_rng_23(rand(),tau);
+
+g_rng_03 = @(tau) exprnd(1000);
+
+g_rng_02 = @(tau) pd_disp.random();
+g_rng_20 = @(tau) pd_t_r.random();
+
+g_rng_absorb = @(tau) 1e8; % A very large number
+
+g_rng = {handle_zero, g_rng_01, g_rng_02, g_rng_03;...
+    g_rng_01, handle_zero, g_rng_02, g_rng_03;...
+    g_rng_20, handle_zero, handle_zero, g_rng_23;...
+    g_rng_absorb, g_rng_absorb, g_rng_absorb,g_rng_absorb};
+
+% c
+c_01 = @(tau) 1;
+c_03 = @(tau) 1;
+c_23 = @(tau) 1;
+
+c_02 = @(tau) 9.9984e-4./(.9984 + .0887024*exp(tau/1000).*(-1 + erf(.05 + .01*tau)));
+c_20 = @(tau) 2.00177/2;
+
+handle_one = @(tau) 1;
+
+c = {handle_zero, c_01, c_02, c_03;...
+    c_01, handle_zero, c_02, c_03;...
+    c_20, handle_zero, handle_zero, c_23;...
+    handle_one,handle_one,handle_one,handle_one};
+
+% Calculate the same probability distribution
+fprintf('\n PDF approach\n')
+p_t_sim = zeros(4,length(t)); % p(t): 4*1, each column corresponds to one state
+IS_NHSMP = 1;
+
+tic;
+
+for i = 1:length(t)
+    fprintf('%d/%d\n',i,length(t));
+    count_0 = 0; count_1 = 0; count_2 = 0; count_3 = 0; % Counter
+    for j = 1:ns
+        [temp_t,y] = simulate_semi_markov_rejection(f_t,g_t,g_rng,c,...
+            P,pi,t(i),IS_NHSMP);
+        switch y(end)
+            case 1
+                count_0 = count_0 + 1;
+            case 2
+                count_1 = count_1 + 1;
+            case 3
+                count_2 = count_2 + 1;
+            case 4
+                count_3 = count_3 + 1;
+        end
+    end
+    p_t_sim(1,i) = count_0/ns;
+    p_t_sim(2,i) = count_1/ns;
+    p_t_sim(3,i) = count_2/ns;
+    p_t_sim(4,i) = count_3/ns;
+end
+
+elapsed_time_2 = toc;
+
+% Display results
+figure(h_p_1);
+plot(t,p_t_sim(1,:),'-dr')
+figure(h_p_2);
+plot(t,p_t_sim(2,:),'-dr')
+figure(h_p_3);
+plot(t,p_t_sim(3,:),'-dr')
+figure(h_p_4);
+plot(t,p_t_sim(4,:),'-dr')
+
+fprintf('The elapsed time for 1 is %f seconds\n',elapsed_time_1);
+fprintf('The elapsed time for 2 is %f seconds\n',elapsed_time_2);
